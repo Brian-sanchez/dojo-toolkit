990b70c96e7a71ae9cc4fd7c14c1f11d67660473
Cesar Fuentes <fuentesnicolas1@hotmail.com>
2022-08-25 22:47:33 -0300

    Cambios 22hs
    Agregar form product en ventana y falta corregir detalles

--- js/form.js
+++ js/form.js
@@ -1,3 +1,37 @@
+// let btn = document.getElementById("enviar")
+// let email = document.getElementById("email")
+// let pass = document.getElementById("password")
+
+// console.log(btn);
+
+// btn.addEventListener("click" , function(e){
+//     e.preventDefault()
+//     // console.log("click");
+//     console.log(email.value);
+//     console.log(pass.value);
+
+// })
+
+// require(["dojo/ready", "dojo/dom", "dojo/_base/connect", "dojo/_base/event" , "dojo/on"], function(ready, dom, connect, event){
+//     ready(function(){
+//         var node = dom.byId("enviar");
+//         // let email = dom.byId("email")
+//         connect.connect(node, "click", function(e){
+//             event.stop(e); // prevents default link execution
+//             console.log("llegue");
+//             // console.log(email);
+//         });
+//     });
+// });
+
+function agregar(){
+    // let email = document.getElementById("fisrt")
+
+    // console.log(email);
+    location.reload()
+    
+}
+
 function formulario() {
     require([
         "dojo/dom",
@@ -11,22 +45,17 @@ function formulario() {
         "dojo/domReady!"
     ], function (dom, parser, Form, Button, ValidationTextBox, DateTextBox, validate, web) {
 
-        var consola = new Button({
-            label: "Valores del formulario",
-            type: "button",
-            onClick: function () {
-                console.log(dijit.byId("myForm").getValues());
-            }
-        }, "consola").startup();
-
         var submit = new Button({
             label: "Submit",
             type: "submit",
             onClick: function (event) {
-                event.preventDefault();
-                let clients = [];
+                // event.preventDefault();
+                let clients = JSON.parse(localStorage.getItem("customers")) || [];
+
+                console.log(clients)   
                 let client = { id: "", first: "", last: "", phone: "", email: "" };
-                client.id = clients.length + 1;
+
+                client.id = (clients.length + 1).toString();
 
                 if (validate) {
                     client.first = dijit.byId("myForm").getValues().first;
@@ -34,13 +63,18 @@ function formulario() {
                     client.phone = dijit.byId("myForm").getValues().phone;
                     client.email = dijit.byId("myForm").getValues().email;
                     clients = [...clients, client];
+                   
+                    localStorage.setItem("customers", JSON.stringify(clients))
+                  
                     console.log(clients);
-                    return confirm('Valid form, press OK to send');
+                    // return confirm('Valid form, press OK to send');
                 } else {
                     alert('The form contains invalid data or missing information!');
                     return false;
                 }
-                return true;
+                
+                location.reload()
+                // return true;
             }
         }, "submit").startup();
 
@@ -53,13 +87,5 @@ function formulario() {
         }, "reset").startup();
     });
 }
-
-
-
-
-
-
-
-
 
 
